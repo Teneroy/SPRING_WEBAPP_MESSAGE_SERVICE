5341cc2b5cfe4616bbf20b36b70f7e7704821a40
Teneroy <alexchernishof@gmail.com>
2020-05-24 19:54:53 +0300

    Initialized project

---
+++ src/main/kotlin/org/example/microservices/messages/MessagesApplication.kt
@@ -0,0 +1,11 @@
+package org.example.microservices.messages
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessagesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MessagesApplication>(*args)
+}
